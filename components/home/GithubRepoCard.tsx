62490c17d0e373c3ed7446a836bdee1152c53370
Michael Rausch <me@michaelrausch.nz>
2022-01-27 18:50:46 +1300

    Fixed import for GithubRepository type

--- components/home/GithubRepoCard.tsx
+++ components/home/GithubRepoCard.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion"
-import { GithubRepository } from "../../pages"
+import { GithubRepository } from "./GithubRepoList"
 
 interface Props {
     repo: GithubRepository
